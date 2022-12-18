6b3ba660886d3827bc05686bbccd76946ffe952e
Yuto Takakura <yutotakakura0320@gmail.com>
2022-12-18 20:32:38 +0900

    feat(setup): add setup.sh

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+
+UNDER_HOMEDIR_FILES=".zshrc .tmux.conf .gitconfig"
+for file in $UNDER_HOMEDIR_FILES
+do
+    if [ -e ~/$file ]; then
+        mv ~/.$file ~/.$file.org
+    fi
+    ln -s ~/dotfiles/$file ~/$file
+done
+
+
+UNDER_DOTCONFIGDIR_FILES="nvim"
+if [ ! -d ~/.config ]; then
+    mkdir ~/.config
+fi
+
+for file in $UNDER_DOTCONFIGDIR_FILES
+do
+    if [ -e ~/.config/$file ]; then
+        mv ~/.config/$file ~/.config/$file.org
+        ln -s ~/dotfiles/.config/$file ~/.config/$file
+    fi
+done
